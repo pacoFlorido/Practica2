6689fdb1a0504d035e209230c20791fc52207653
Paco Florido <franciscoflorido0@gmail.com>
2023-10-18 20:36:12 +0200

    Creando layout de activity_agenda.xml

---
+++ app/src/main/java/net/iesochoa/pacofloridoquesada/practica2/Agenda.kt
@@ -0,0 +1,11 @@
+package net.iesochoa.pacofloridoquesada.practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Agenda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
